fb6ef68268f88b3156b3df448f7e36d3c923b586
unknown <tanthanh112001@gmail.com>
2025-02-10 15:16:12 +0700

    Adjust GA4 tracking to comply with Chrome's cookie policy

--- wwwroot/js/gtag.js
+++ wwwroot/js/gtag.js
@@ -9,7 +9,7 @@
     gtag('js', new Date());
     gtag('config', 'G-1PHCN6YN6R', {
         'anonymize_ip': true,
-        'cookie_flags': 'SameSite=None;Secure',
+        'cookie_flags': 'Secure;SameSite=Lax',
         'cookie_expires': 63072000,
         'send_page_view': true
     });
